dddadfec755272d740aa2b0bbf209103619ee371
cadecameron <cade.cameron@gmail.com>
2020-02-24 12:14:39 -0800

    UPDATED proxy to use InnCognito group proxy code.

--- server/server.js
+++ server/server.js
@@ -1,29 +1,24 @@
 // require('newrelic');
 const express = require('express');
-const proxy = require('http-proxy-middleware');
-
+const axios = require('axios');
 const PORT = 3000;
+
 const app = express();
+
 
 const dirPath = `${__dirname}/../public/dist/`;
 app.use(express.static(dirPath));
 
 const { routes } = require('../proxyconfig.json');
 
-// TODO: fix this redirect to static to correctly handle URL paramater
-app.use('/:id', express.static(dirPath));
-
-for (route of routes) {
-  app.use(route.route,
-      proxy({
-          target: route.address,
-            //   No longer required - proxyconfig.json modified
-            //   pathRewrite: (path, req) => {
-            //     return path.split('/').slice(2).join('/');
-            //   }
-      })
-  );
-}
-
+app.get('/api/:component/:id', (req, res) => {
+  const { component, id } = req.params;
+  axios.get(`${routes[component].address}/${id}`)
+    .then(data => res.status(200).send(data.data))
+    .catch(e => {
+      console.log(e);
+      res.status(500).send('Error');
+    });
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
