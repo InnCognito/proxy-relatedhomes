25b81e3a0512a5fcecbeadddf342bdd0e6f780be
cadecameron <cade.cameron@gmail.com>
2020-02-24 09:25:21 -0800

    UPDATED reverse proxy to fix configuration for URL param redirects.

--- server/server.js
+++ server/server.js
@@ -10,13 +10,17 @@ app.use(express.static(dirPath));
 
 const { routes } = require('../proxyconfig.json');
 
+// TODO: fix this redirect to static to correctly handle URL paramater
+app.use('/:id', express.static(dirPath));
+
 for (route of routes) {
   app.use(route.route,
       proxy({
           target: route.address,
-          pathRewrite: (path, req) => {
-              return path.split('/').slice(2).join('/');
-          }
+            //   No longer required - proxyconfig.json modified
+            //   pathRewrite: (path, req) => {
+            //     return path.split('/').slice(2).join('/');
+            //   }
       })
   );
 }
