101e6c6ce5e71d8d13ee4d4d34a2d44a97e7d54e
cadecameron <cade.cameron@gmail.com>
2020-02-28 16:04:43 -0800

    UPDATED proxy for stress testing

--- server/server.js
+++ server/server.js
@@ -21,4 +21,22 @@ app.get('/api/:component/:id', (req, res) => {
     });
 });
 
+app.get('/api/:id', (req, res) => {
+  const { id } = req.params;
+
+  axios.all([
+    axios.get(`${routes.morehomes.address}/${id}`),
+    axios.get(`${routes.reviews.address}/${id}`),
+    axios.get(`${routes.listings.address}/${id}`)
+  ])
+  .then(axios.spread((morehomes, reviews, listings) => {
+    let responseString = '[' + JSON.stringify(morehomes.data) + ',' + JSON.stringify(reviews.data) + ',' + JSON.stringify(listings.data) + ']';
+    res.status(200).send(responseString);
+  }))
+  .catch((err) => {
+    console.log('Error occured getting subrequests', err);
+    res.status(500).send('Error getting subrequests from api services');
+  })
+});
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
